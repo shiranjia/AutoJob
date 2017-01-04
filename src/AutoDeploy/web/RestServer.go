db98e081e556c1bc95fe8a7749b4eff28915b137
jiashiran <jiashiran@jd.com>
2017-01-04 16:40:33 +0800

    rest api

--- src/AutoDeploy/web/RestServer.go
+++ src/AutoDeploy/web/RestServer.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"log"
 	"io"
+	"fmt"
+	"strings"
 )
 
 type Rest struct {
@@ -27,6 +29,11 @@ type Jobs struct {
 func restIndex(res http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 	name := globalSessions.SessionStart(res, req).Get("name")
+	log.Println(req)
+	for k, v := range req.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
 	jobs := job.Read()
 	set := false
 	for _,v := range jobs {
@@ -52,13 +59,45 @@ func restIndex(res http.ResponseWriter, req *http.Request) {
 }
 
 func restSaveOrUpdate(res http.ResponseWriter, req *http.Request) {
-	j := toJob(req)
-	if j.Name == ""{
-		io.WriteString(res, "name must not null")
+	res.Header().Add("Access-Control-Allow-Origin","*")
+	defer func(){
+		if err:=recover();err!=nil{
+			log.Println("restSaveOrUpdate.err:",err)
+			io.WriteString(res,"err")
+		}
+	}()
+	req.ParseForm()
+	jobJson := req.Form.Get("job")
+	//log.Println("jobJson:",jobJson)
+	var _job *job.DeployJob = new(job.DeployJob)
+	err := json.Unmarshal([]byte(jobJson),_job)
+	if err != nil{
+		log.Println("err:",err)
 	}
-	job.SaveOrUpdate(&j)
-	session := globalSessions.SessionStart(res, req)
-	session.Set("name",j.Name)
-	http.Redirect(res,req,"/" ,http.StatusMovedPermanently)
-	return
+	logJob(_job)
+	job.SaveOrUpdate(_job)
+	io.WriteString(res,"ok")
+}
+
+func logJob(job *job.DeployJob)  {
+	log.Println("Name:",job.Name)
+	log.Println("Config:",job.Config)
+	localBefore := job.LocalBefore
+	for _,l:=range localBefore {
+		log.Println("localBefore:",l)
+	}
+	RemoteBefore := job.RemoteBefore
+	for _,l:=range RemoteBefore {
+		log.Println("RemoteBefore:",l)
+	}
+	UploadJob := job.UploadJob
+	log.Println("UploadJob:",UploadJob)
+	LocalAfter := job.LocalAfter
+	for _,l:=range LocalAfter {
+		log.Println("LocalAfter:",l)
+	}
+	RemoteAfter := job.RemoteAfter
+	for _,l:=range RemoteAfter {
+		log.Println("RemoteAfter:",l)
+	}
 }
